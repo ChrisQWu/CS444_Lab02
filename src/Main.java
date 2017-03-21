27c1203854c4ee63747afb1ad3e2fc16e243ac98
ChrisWuUNM <chris080695@gmail.com>
2017-03-20 19:19:20 -0600

    Added methods in main to help with adding and removing padding

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by thebaker on 3/10/17.
  */
@@ -24,45 +21,73 @@ public class Main {
 
     }
 
-    public int hexStringtoDecimal(String iv) {
+    private int hexStringtoDecimal(String iv) {
         return Integer.decode(iv);
     }
 
-    public String decimaltoHexString(int mask) {
+    private String decimaltoHexString(int mask) {
         return Integer.toHexString(mask);
     }
 
-    public void blockByBlock()
+    private void blockByBlock()
     {
-        String encryptedText = Constants.ENCRYPTED_TEXT;
         String modIV = "";
-        String iv = encryptedText.substring(encryptedText.length()-32,encryptedText.length());
+        String iv;
         String msg;
-        for (int i = encryptedText.length()-32; i > 32; i-=32) {
-            msg = iv;
-            iv = encryptedText.substring(i-32,i);
+        for (int i=1; i < Constants.ENCRYPTED_TEXT.length;i++) {
+            iv = Constants.ENCRYPTED_TEXT[i-1];
+            msg = Constants.ENCRYPTED_TEXT[i];
             modIV = byteByByte(iv,msg)+modIV;
+            System.out.println("Mod IV : "+modIV);
             System.out.println("IV     : "+iv);
             System.out.println("Message: "+msg);
         }
     }
 
-    public String byteByByte(String iv, String msg)
+    private String byteByByte(String iv, String msg)
     {
         String modIV ="";
         for (int i = 32, pad = 1; i>0 ; i-=2, pad++) {//byte by byte for-loop starting from the right
             String ivByte = iv.substring(i-2, i);
-            String msgByte = msg.substring(i-2, i);
-            int ivVal = hexStringtoDecimal(Constants.HEX+ivByte);
-            int msgVal = hexStringtoDecimal(Constants.HEX+msgByte);
-            for (int j = 0; j < 256; j++) {
-                if(j!=ivVal && (ivVal^j)==pad) {
-                    System.out.println("Pad: "+pad+" "+ivVal + " xor " + j + " " + (ivVal ^ j));
+            for (int j = 0; j < 256; j++) {//find the proper R value
+                String tempIV = iv.substring(0,i-2) + decimaltoHexString(j) + padModify(modIV);
+                if(CommandLine.client(tempIV+msg)) {
+                    modIV = decimaltoHexString(j) + modIV;
+                    break;
                 }
             }
-            modIV = ivVal+modIV;
+
 //            System.out.println("String: "+ivByte+" "+ivVal);
         }
         return modIV;
+    }
+
+    /**
+     * Takes in the String of my modifications and change them so that they suit the current padding
+     * i.e. modIV = 0x4a5d
+     * The padding = modIV.length()/2 + 1 = 2 + 1 = 3. For the next byte to be modified
+     * The first byte will be 0x5d. This byte was modified by me to obtain a padding of 0x01.
+     *   To change this modified 0x5d to obtain a padding of 0x03 = 0x01 ^ (?)
+     *   (?) = 0x01 ^ 0x03 = 0x02
+     *   Therefore: 0x5d ^ 0x02 = 0x5f
+     * The second byte will be 0x4a. This byte was modified by me to obtain a padding of 0x02.
+     *   To change this modified 0x4a to obtain a padding of 0x03 = 0x02 ^ (?)
+     *   (?) = 0x02 ^ 0x03 = 0x01
+     *   Therefore: 0x4a ^ 0x01 = 0x4b
+     * To generalize: new byte = old byte ^ its padding ^ target padding
+     * @param modIV
+     * @return A modIV that is appropiate for the next padding target
+     */
+    private String padModify(String modIV)
+    {
+        if(modIV.isEmpty())return "";
+        String padIV = "";
+        int targetPad = modIV.length()/2;
+        for (int i = modIV.length(), pad = 1; i > 2; i-=2, pad++) {
+            int tempByte = hexStringtoDecimal(Constants.HEX+modIV.substring(i-2,i));
+            tempByte ^= pad ^ targetPad;
+            padIV = decimaltoHexString(tempByte) + padIV;
+        }
+        return padIV;
     }
 }
